0821434cf8a6b7759c099473e648d1798c4399df
gomDarachi <ttcoristory@naver.com>
2025-03-31 04:56:09 +0900

    feat: Home Page 컨텐츠 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+// app/page.tsx
 export default function Home() {
-  return <div>contents</div>;
+  return (
+    <div className="flex justify-center items-center h-full text-center">
+      <div>
+        <h1 className="text-2xl font-bold mb-4">
+          아직 가지고 계신 반이 없으시군요!
+        </h1>
+        <p className="text-lg mb-6">
+          왼쪽 아래 반 추가 버튼을 사용해 반을 만들어보세요!
+        </p>
+      </div>
+    </div>
+  );
 }
